0548cc341e5799f4785063219a013ce5611785e5
jpsm83 <jpsm83@hotmail.com>
2021-09-11 14:02:21 +0200

    done

--- src/components/RGBColorPicker/RGBColorPicker.js
+++ src/components/RGBColorPicker/RGBColorPicker.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { TextField, Button } from '@material-ui/core';
 
 function RGBColorPicker({onChangeColor}) {
     return (
         <div>
             <form onSubmit={onChangeColor} className="color-form-container">
-                <TextField type="number" variant="filled" color="primary" label="red" name="red"/>
-                <TextField type="number" variant="filled" color="primary" label="green" name="green"/>
-                <TextField type="number" variant="filled" color="primary" label="blue" name="blue"/>
-                <Button type="sumbit">Submit</Button>  
+                <input type="number" placeholder="red" name="red"/>
+                <input type="number" placeholder="green" name="green"/>
+                <input type="number" placeholder="blue" name="blue"/>
+                <button type="sumbit">Submit</button>  
             </form>
         </div>
     )
--- src/components/SingleColorPicker/SingleColorPicker.js
+++ src/components/SingleColorPicker/SingleColorPicker.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
-// import RGBColorPicker from '../RGBColorPicker/RGBColorPicker';
+import React, { useState } from "react";
+import RGBColorPicker from "../RGBColorPicker/RGBColorPicker";
 
 function SingleColorPicker() {
-    const [ colors, setColors ] = useState("122,133,133")
+  const [colors, setColors] = useState("122,133,133");
 
-    const handleChangeColor = (event) => {
-        event.preventDefault();
-        console.log(event.target.red.value);
-        // setColors([event.target.red.value, event.target.green.value, event.target.blue.value]);
-        setColors(`${event.target.red.value}, ${event.target.green.value}, ${event.target.blue.value}`);
-    }
+  const handleChangeColor = (event) => {
+    event.preventDefault();
+    setColors(
+      `${event.target.red.value},${event.target.green.value},${event.target.blue.value}`
+    );
+  };
 
-    return (
-        <div>
-            {/* <RGBColorPicker  onChangeColor={handleChangeColor} /> */}
-            <div style={{background: `rgb(${colors})`}} class="show-rgb-color"></div>
-            <p>rgb({colors})</p>
-        </div>
-    )
+  return (
+    <div>
+      <RGBColorPicker onChangeColor={(event) => handleChangeColor(event)} />
+      <div style={{ background: `rgb(${colors})` }} class="show-rgb-color">
+        <p>rgb({colors})</p>
+      </div>
+    </div>
+  );
 }
 
 export default SingleColorPicker;
